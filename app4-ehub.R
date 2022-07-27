d53a102d841162a0862843ccba36a9605689cd6e
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-07-27 22:33:00 +0100

    start app for ExperimentHub

---
+++ app4-ehub.R
@@ -0,0 +1,74 @@
+library(ExperimentHub)
+
+# lpfun ----
+
+lpfun <- function() {
+    ehub <- ExperimentHub()
+    
+    datasets_available_table <- as.data.frame(mcols(ehub))
+    
+    se_load <- function(x) {
+        stop("TODO")
+    }
+    
+    function (FUN, input, output, session) {
+        .ui_dataset_table <- "datasets_table"
+        .ui_launch_button <- "launch"
+        .dataset_selected_index <- paste0(.ui_dataset_table, "_rows_selected")
+        
+        # nocov start
+        output$allPanels <- renderUI({
+            tagList(
+                fluidRow(
+                    column(width = 6L, shinydashboard::box(title = "ExperimentHub",
+                      collapsible = FALSE, width = NULL, DTOutput(.ui_dataset_table)
+                    ))
+                ),
+                fluidRow(column(width = 12L, actionButton(.ui_launch_button, label="Launch", style="color: #ffffff; background-color: #0092AC; border-color: #2e6da4")))
+            )
+        })
+        # nocov end
+        
+        launcherObjects <- new.env()
+        
+        observeEvent(input[[.dataset_selected_index]], {
+            launcherObjects[[.dataset_selected_index]] <- input[[.dataset_selected_index]]
+            
+        }, ignoreInit = FALSE, ignoreNULL = FALSE)
+        
+        output[[.ui_dataset_table]] <- DT::renderDT({
+            DT::datatable(datasets_available_table, filter="top", rownames=FALSE,
+            options=list(
+                search=list(search="", smart=FALSE, regex=TRUE, caseInsensitive=FALSE),
+                searchCols=c(list(NULL), list(NULL)), # row names are the first column!
+                scrollX=TRUE,
+                columnDefs=NULL),
+            selection=list(mode = 'single', selected=1L, target = 'row'))
+        }, server = FALSE)
+
+        # nocov start, ignoreNULL=TRUE, ignoreInit=TRUE
+        observeEvent(input[[.ui_launch_button]], {
+            se2 <- try(se_load(launcherObjects[[.dataset_selected_index]]))
+            if (is(se2, "try-error")) {
+                showNotification("invalid SummarizedExperiment supplied", type="error")
+            } else {
+                # init <- try(initLoad(input[[.initializeInitial]]))
+                # if (is(init, "try-error")) {
+                #     showNotification("invalid initial state supplied", type="warning")
+                #     init <- NULL
+                # }
+                # init <- list(ReducedDimensionPlot())
+                init <- NULL
+                FUN(SE=se2, INITIAL=init)
+            }
+        }, ignoreNULL=TRUE, ignoreInit=TRUE)
+        # nocov end
+
+        invisible(NULL)
+    }
+}
+
+app <- iSEE(landingPage=lpfun())
+if (interactive()) {
+  shiny::runApp(app, port=1234, launch.browser = TRUE)
+}

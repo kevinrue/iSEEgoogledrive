41ae224cc74331cbae39898cc34053b9cff89ce1
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-07-28 11:06:29 +0100

    add selectInput to restrict columns in dataset table

--- app4-ehub.R
+++ app4-ehub.R
@@ -15,13 +15,16 @@ lpfun <- function() {
         .ui_dataset_table <- "datasets_table"
         .ui_launch_button <- "launch"
         .dataset_selected_index <- paste0(.ui_dataset_table, "_rows_selected")
+        .ui_dataset_columns <- "datasets_columns"
         
         # nocov start
         output$allPanels <- renderUI({
             tagList(
                 fluidRow(
-                    column(width = 6L, shinydashboard::box(title = "ExperimentHub",
-                      collapsible = FALSE, width = NULL, DTOutput(.ui_dataset_table)
+                    column(width = 8L, shinydashboard::box(title = "ExperimentHub",
+                      collapsible = FALSE, width = NULL,
+                        selectInput(inputId = .ui_dataset_columns, label = "Show columns:", choices = colnames(datasets_available_table), selected = c("title", "dataprovider", "species"), multiple = TRUE),
+                        DTOutput(.ui_dataset_table)
                     ))
                 ),
                 fluidRow(column(width = 12L, actionButton(.ui_launch_button, label="Launch", style="color: #ffffff; background-color: #0092AC; border-color: #2e6da4")))
@@ -29,26 +32,34 @@ lpfun <- function() {
         })
         # nocov end
         
-        launcherObjects <- new.env()
+        pObjects <- new.env()
+        rObjects <- reactiveValues(rerender_datasets=1L)
         
         observeEvent(input[[.dataset_selected_index]], {
-            launcherObjects[[.dataset_selected_index]] <- input[[.dataset_selected_index]]
+            pObjects[[.dataset_selected_index]] <- input[[.dataset_selected_index]]
             
         }, ignoreInit = FALSE, ignoreNULL = FALSE)
         
+        observeEvent(input[[.ui_dataset_columns]], {
+            pObjects[[.ui_dataset_columns]] <- input[[.ui_dataset_columns]]
+            rObjects$rerender_datasets <- iSEE:::.increment_counter(isolate(rObjects$rerender_datasets))
+        })
+        
         output[[.ui_dataset_table]] <- DT::renderDT({
-            DT::datatable(datasets_available_table, filter="top", rownames=FALSE,
+            force(rObjects$rerender_datasets)
+            datasets_table_visible <- datasets_available_table[, pObjects[[.ui_dataset_columns]]]
+            DT::datatable(datasets_table_visible, filter="top", rownames=FALSE,
             options=list(
                 search=list(search="", smart=FALSE, regex=TRUE, caseInsensitive=FALSE),
                 searchCols=c(list(NULL), list(NULL)), # row names are the first column!
                 scrollX=TRUE,
                 columnDefs=NULL),
             selection=list(mode = 'single', selected=1L, target = 'row'))
-        }, server = FALSE)
+        })
 
         # nocov start, ignoreNULL=TRUE, ignoreInit=TRUE
         observeEvent(input[[.ui_launch_button]], {
-            se2 <- try(se_load(launcherObjects[[.dataset_selected_index]]))
+            se2 <- try(se_load(pObjects[[.dataset_selected_index]]))
             if (is(se2, "try-error")) {
                 showNotification("invalid SummarizedExperiment supplied", type="error")
             } else {

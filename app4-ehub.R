003b35df7109a839fb2907b5616cd73e3eac54b7
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-07-28 11:29:29 +0100

    add panel with markdown overview

--- app4-ehub.R
+++ app4-ehub.R
@@ -16,15 +16,20 @@ lpfun <- function() {
         .ui_launch_button <- "launch"
         .dataset_selected_index <- paste0(.ui_dataset_table, "_rows_selected")
         .ui_dataset_columns <- "datasets_columns"
+        .ui_markdown_overview <- "markdown_overview"
         
         # nocov start
         output$allPanels <- renderUI({
             tagList(
                 fluidRow(
-                    column(width = 8L, shinydashboard::box(title = "ExperimentHub",
+                    column(width = 7L, shinydashboard::box(title = "ExperimentHub",
                       collapsible = FALSE, width = NULL,
                         selectInput(inputId = .ui_dataset_columns, label = "Show columns:", choices = colnames(datasets_available_table), selected = c("title", "dataprovider", "species"), multiple = TRUE),
                         DTOutput(.ui_dataset_table)
+                    )),
+                    column(width = 5L, shinydashboard::box(title = "Overview",
+                      collapsible = FALSE, width = NULL,
+                        uiOutput(.ui_markdown_overview)
                     ))
                 ),
                 fluidRow(column(width = 12L, actionButton(.ui_launch_button, label="Launch", style="color: #ffffff; background-color: #0092AC; border-color: #2e6da4")))
@@ -39,6 +44,35 @@ lpfun <- function() {
             pObjects[[.dataset_selected_index]] <- input[[.dataset_selected_index]]
             
         }, ignoreInit = FALSE, ignoreNULL = FALSE)
+        
+        output[[.ui_markdown_overview]] <- renderUI({
+            dataset_selected_index <- input[[.dataset_selected_index]]
+            if (is.null(dataset_selected_index)) {
+                contents <- markdown("")
+            } else {
+                ehub_selected <- ehub[dataset_selected_index]
+                contents <- markdown(paste0(
+                    "# ", ehub_selected$title, "\n\n",
+                    "- **Data provider:** ", ehub_selected$dataprovider, "\n\n",
+                    "- **Species:** ", ehub_selected$species, "\n\n",
+                    "- **Taxonomy ID:** ", ehub_selected$taxonomyid, "\n\n",
+                    "- **Genome:** ", ehub_selected$genome, "\n\n",
+                    "## Description", "\n\n", ehub_selected$description, "\n\n",
+                    "## Details", "\n\n",
+                    "- **Coordinate 1-based:** ", as.logical(ehub_selected$coordinate_1_based), "\n\n",
+                    "- **Maintainer:** ", ehub_selected$maintainer, "\n\n",
+                    "- **Date added:** ", ehub_selected$rdatadateadded, "\n\n",
+                    "- **Preparer class:** ", ehub_selected$preparerclass, "\n\n",
+                    "- **R data class:** ", ehub_selected$rdataclass, "\n\n",
+                    "- **R data path:** ", ehub_selected$rdatapath, "\n\n",
+                    "- **Source URL:** ", ehub_selected$sourceurl, "\n\n",
+                    "- **Source type:** ", ehub_selected$sourcetype, "\n\n",
+                    "## Tags", "\n\n",
+                    paste0(sprintf("- %s", strsplit(x$tags, ", ")[[1]]), collapse = "\n")
+                ))
+            }
+            contents
+        })
         
         observeEvent(input[[.ui_dataset_columns]], {
             pObjects[[.ui_dataset_columns]] <- input[[.ui_dataset_columns]]
